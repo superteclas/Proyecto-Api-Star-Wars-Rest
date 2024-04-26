c6d860457913b3480ec952968d70d8dab28b50b9
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-04-26 21:16:34 +0000

    empezando con los tokens

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Characters, Planets, Vehicles, CharactersFavorites, PlanetsFavorites, VehiclesFavorites
@@ -30,6 +31,10 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
 # Manejo de errores 
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -40,6 +45,18 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#----------------------------METODO EJEMPLO----------------------------------------------------------------------------------------------------------------------------------------------
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
 
 
 #----------------------------METODO USER----------------------------------------------------------------------------------------------------------------------------------------------
