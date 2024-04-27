36f6527aa8a01189fd23eb621dc1fe5153d8e88c
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-04-27 10:19:06 +0000

    funcionan las importaciones de JWT_EXTENDED

--- src/app.py
+++ src/app.py
@@ -5,10 +5,10 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-""" from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
-from flask_jwt_extended import JWTManager """
+from flask_jwt_extended import JWTManager 
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Characters, Planets, Vehicles, CharactersFavorites, PlanetsFavorites, VehiclesFavorites
@@ -19,7 +19,9 @@ app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
 
 
 #Configuracion de la base de datos
@@ -38,9 +40,6 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Setup the Flask-JWT-Extended extension
-#app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
-#jwt = JWTManager(app)
 
 # Manejo de errores 
 @app.errorhandler(APIException)
@@ -55,7 +54,7 @@ def sitemap():
 #----------------------------METODO EJEMPLO----------------------------------------------------------------------------------------------------------------------------------------------
 # Create a route to authenticate your users and return JWTs. The
 # create_access_token() function is used to actually generate the JWT.
-""" @app.route("/login", methods=["POST"])
+@app.route("/login", methods=["POST"])
 def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
@@ -63,7 +62,7 @@ def login():
         return jsonify({"msg": "Bad username or password"}), 401
 
     access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token) """
+    return jsonify(access_token=access_token)
 
 
 #----------------------------METODO USER----------------------------------------------------------------------------------------------------------------------------------------------
