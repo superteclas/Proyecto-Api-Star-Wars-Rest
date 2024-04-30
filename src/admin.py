f11d8a96993d260c66222f9f04c393985e0cf581
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-04-30 09:39:35 +0000

    arregladas columnas favoritos del ADMIN

--- src/admin.py
+++ src/admin.py
@@ -9,13 +9,25 @@ def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
     admin = Admin(app, name='Superteclas Admin', template_mode='bootstrap3')
+
+    class MyFavoriteViewCharacters(ModelView):
+        column_list = ( 'character_id', 'user_id',)
+        form_columns = ('character_id', 'user_id',)
+    class MyFavoriteViewPlanets(ModelView):
+        column_list = ( 'planet_id', 'user_id',)
+        form_columns = ('planet_id', 'user_id',)
+
+    class MyFavoriteViewVehicles(ModelView):
+        column_list = ( 'vehicle_id', 'user_id',)
+        form_columns = ('vehicle_id', 'user_id',)
+
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Characters, db.session))
     admin.add_view(ModelView(Planets, db.session))
     admin.add_view(ModelView(Vehicles, db.session))
-    admin.add_view(ModelView (CharactersFavorites, db.session))
-    admin.add_view(ModelView(PlanetsFavorites, db.session))
-    admin.add_view(ModelView(VehiclesFavorites, db.session))
+    admin.add_view(MyFavoriteViewCharacters(CharactersFavorites, db.session))  # Fix: Added closing parenthesis
+    admin.add_view(MyFavoriteViewPlanets(PlanetsFavorites, db.session))
+    admin.add_view(MyFavoriteViewVehicles(VehiclesFavorites, db.session))
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session)) 
