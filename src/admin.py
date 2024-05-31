5c7bbce500f6d8fa2b6c45cafdf9df0cb3837bd0
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-05-31 10:32:43 +0000

    cambiada logica

--- src/admin.py
+++ src/admin.py
@@ -1,35 +1,27 @@
 import os
 from flask_admin import Admin
-from models import db, User, Characters, Planets,Vehicles, CharactersFavorites, PlanetsFavorites, VehiclesFavorites
+from models import db, User, Planet, Character, Vehicle, FavoritePlanet, FavoriteCharacter, FavoriteVehicle
 from flask_admin.contrib.sqla import ModelView
 
-
-#Esta función se encarga de configurar el admin de la aplicación
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='Superteclas Admin', template_mode='bootstrap3')
-#Esta clase se encarga de mostrar los datos de la tabla CharactersFavorites en el admin de la aplicación
-    class MyFavoriteViewCharacters(ModelView):
-        column_list = ( 'character_id', 'user_id',)
-        form_columns = ('character_id', 'user_id',)
-    class MyFavoriteViewPlanets(ModelView):
-        column_list = ( 'planet_id', 'user_id',)
-        form_columns = ('planet_id', 'user_id',)
+    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
 
-    class MyFavoriteViewVehicles(ModelView):
-        column_list = ( 'vehicle_id', 'user_id',)
-        form_columns = ('vehicle_id', 'user_id',)
+    class FavoriteCharacterView(ModelView):
+        column_list=('character_id', 'user_id')
+        form_columns=('character_id', 'user_id')
+    class FavoritePlanetView(ModelView):
+        column_list=('planet_id', 'user_id')
+        form_columns=('planet_id', 'user_id')
+    class FavoriteVehicleView(ModelView):
+        column_list=('vehicle_id', 'user_id')
+        form_columns=('vehicle_id', 'user_id')
 
-    # Esta función se encarga de agregar las tablas al admin de la aplicación
     admin.add_view(ModelView(User, db.session))
-    admin.add_view(ModelView(Characters, db.session))
-    admin.add_view(ModelView(Planets, db.session))
-    admin.add_view(ModelView(Vehicles, db.session))
-    admin.add_view(MyFavoriteViewCharacters(CharactersFavorites, db.session))  # Fix: Added closing parenthesis
-    admin.add_view(MyFavoriteViewPlanets(PlanetsFavorites, db.session))
-    admin.add_view(MyFavoriteViewVehicles(VehiclesFavorites, db.session))
-    
-
-    # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session)) 
+    admin.add_view(ModelView(Character, db.session))
+    admin.add_view(ModelView(Planet, db.session))
+    admin.add_view(ModelView(Vehicle, db.session))
+    admin.add_view(FavoriteCharacterView(FavoriteCharacter, db.session))
+    admin.add_view(FavoritePlanetView(FavoritePlanet, db.session))
+    admin.add_view(FavoriteVehicleView(FavoriteVehicle, db.session))
--- src/models.py
+++ src/models.py
@@ -1,18 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
+
 db = SQLAlchemy()
 
-
-
-#----------MODELOS DE USER, CHARACTERS, PLANETS, VEHICLES---------------------------------
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    characters_favorites = db.relationship('CharactersFavorites', backref='user', lazy=True)
-    planets_favorites = relationship('PlanetsFavorites', backref='user', lazy=True)
-    vehicles_favorites = relationship('VehiclesFavorites', backref='user', lazy=True)
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True) 
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    favorites_characters = db.relationship('FavoriteCharacter', backref='user', lazy=True)
+    favorites_planets = db.relationship('FavoritePlanet', backref='user', lazy=True)
+    favorites_vehicles = db.relationship('FavoriteVehicle', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.email
@@ -21,21 +18,14 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email
-            
-            # do not serialize the password, its a security breach
         }
-    
-class Characters(db.Model):
+
+class Character(db.Model):
+    __tablename__ = 'character'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    height = db.Column(db.Integer, nullable=False)
-    mass = db.Column(db.Integer, unique=False, nullable=False)
-    eye_color = db.Column(db.String(80), unique=False, nullable=False)
-    hair_color = db.Column(db.String(80), unique=False, nullable=False)
-    birth_year = db.Column(db.String(80), unique=False, nullable=False)
-    skin_color = db.Column(db.String(80), unique=False, nullable=False)
-    gender = db.Column(db.String(80), unique=False, nullable=False)
-    characters_favorites = db.relationship('CharactersFavorites', backref='characters', lazy=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(500), nullable=True)
+    favorites_characters = db.relationship('FavoriteCharacter', backref='character', lazy=True)
 
     def __repr__(self):
         return '<Character %r>' % self.name
@@ -44,120 +34,90 @@ class Characters(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "height": self.height,
-            "mass": self.mass,
-            "eye color": self.eye_color,
-            "hair color": self.hair_color,
-            "birth_year": self.birth_year,
-            "skin color": self.skin_color,
-            "gender": self.gender
-            # do not serialize the password, its a security breach
+            "description": self.description
         }
-class Planets(db.Model):
+
+class FavoriteCharacter(db.Model):
+    __tablename__ = 'favorite_character'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    diameter = db.Column(db.Integer, nullable=False)
-    rotation_speed = db.Column(db.Integer, unique=False, nullable=False)
-    orbital_period = db.Column(db.String(80), unique=False, nullable=False)
-    gravity = db.Column(db.String(80), unique=False, nullable=False)
-    population = db.Column(db.String(80), unique=False, nullable=False)
-    climate = db.Column(db.String(80), unique=False, nullable=False)
-    terrain = db.Column(db.String(80), unique=False, nullable=False)
-    surface_water = db.Column(db.String(80), unique=False, nullable=False)
-    planets_favorites = db.relationship('PlanetsFavorites', backref='planets', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
 
     def __repr__(self):
-        return '<Planets %r>' % self.name
+        return '<FavoriteCharacter %r>' % self.id
+
+    def serialize(self):
+        result = Character.query.filter_by(id=self.character_id).first()
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "character_id": result.serialize()["name"]
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(500), nullable=True)
+    favorites_planets = db.relationship('FavoritePlanet', backref='planet', lazy=True)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
-            "diameter": self.diameter,
-            "rotation_speed": self.rotation_speed,
-            "orbital_period": self.orbital_period,
-            "gravity": self.gravity,
-            "population": self.population,
-            "climate": self.climate,
-            "terrain": self.terrain,
-            "surface_water": self.surface_water
-            # do not serialize the password, its a security breach
-        }
-    
-class Vehicles(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    model = db.Column(db.String(120), nullable=False)
-    vehicle_class = db.Column(db.String, nullable=False)
-    manufacturer = db.Column(db.String, unique=False, nullable=False)
-    cost_in_credits = db.Column(db.String(80), unique=False, nullable=False)
-    lenght = db.Column(db.String(80), unique=False, nullable=False)
-    crew = db.Column(db.String(80), unique=False, nullable=False)
-    passengers = db.Column(db.String(80), unique=False, nullable=False)
-    max_speed = db.Column(db.String(80), unique=False, nullable=False)
-    cargo_capacity = db.Column(db.String(80), unique=False, nullable=False)
-    consumables = db.Column(db.String(80), unique=False, nullable=False)
-    vehicles_favorites = db.relationship('VehiclesFavorites', backref='vehicles', lazy=True)
-
-    def __repr__(self):
-        return '<Vehicles %r>' % self.model
-
-    def serialize(self):
-        return {
-          "id": self.id,
-         "model": self.model,
-            "vehicle_class": self.vehicle_class,
-            "manufacturer": self.manufacturer,
-            "cost_in_credits": self.cost_in_credits,
-            "lenght": self.lenght,
-            "crew": self.crew,
-            "passengers": self.passengers,
-            "max_speed": self.max_speed,
-            "cargo_capacity": self.cargo_capacity,
-            "consumables": self.consumables
+            "description": self.description
         }
 
- #----------MODELOS DE FAVORITOS---------------------------------   
-    
-
-class CharactersFavorites(db.Model):
+class FavoritePlanet(db.Model):
+    __tablename__ = 'favorite_planet'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
 
     def __repr__(self):
-        return '<CharactersFavorites %r>' % self.id
+        return '<FavoritePlanet %r>' % self.id
 
     def serialize(self):
+        result = Planet.query.filter_by(id=self.planet_id).first()
         return {
             "id": self.id,
             "user_id": self.user_id,
-            # do not serialize the password, it's a security breach
+            "planet_id": result.serialize()["name"]
         }
-        
-class PlanetsFavorites(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+class Vehicle(db.Model):
+    __tablename__ = 'vehicle'
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(500), nullable=True)
+    favorites_vehicles = db.relationship('FavoriteVehicle', backref='vehicle', lazy=True)
 
     def __repr__(self):
-        return '<PlanetsFavorites %r>' % self.id
+        return '<Vehicle %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            # do not serialize the password, it's a security breach
+            "name": self.name,
+            "description": self.description
         }
-        
-class VehiclesFavorites(db.Model):
+
+class FavoriteVehicle(db.Model):
+    __tablename__ = 'favorite_vehicle'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
 
     def __repr__(self):
-        return '<VehiclesFavorites %r>' % self.id
+        return '<FavoriteVehicle %r>' % self.id
 
     def serialize(self):
+        result = Vehicle.query.filter_by(id=self.vehicle_id).first()
         return {
             "id": self.id,
-            # do not serialize the password, it's a security breach
+            "user_id": self.user_id,
+            "vehicle_id": result.serialize()["name"]
         }
